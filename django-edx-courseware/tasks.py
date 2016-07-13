ec47a67e94b71ff95bb009fa95c47c76c21553da
Akif Vohra <akifvohra@gmail.com>
2016-07-13 12:38:34 +0000

    Staff notification changes as per teamwork comments

--- django-edx-courseware/tasks.py
+++ django-edx-courseware/tasks.py
@@ -1,6 +1,7 @@
 __author__ = 'Jay Modi'
 
 import logging
+import json
 from xmodule.modulestore.django import modulestore
 
 # ORA imports
@@ -9,6 +10,8 @@ from student.models import CourseAccessRole
 from django.core.mail import EmailMessage
 from openedx.core.djangoapps.content.course_overviews.models import \
     CourseOverview
+from openedx.core.djangoapps.content.course_structures.models import \
+    CourseStructure
 from openassessment.workflow import api
 from edxmako.shortcuts import render_to_string
 from xmodule.modulestore.search import path_to_location
@@ -31,6 +34,7 @@ def staff_notification():
             assessment_data = AssessmentWorkflow.objects.filter(
                 course_id=cid.id)
             item_data = []
+            
             for sid in assessment_data:
                 if not bool(staff.get_latest_staff_assessment(sid.submission_uuid)):
                     if sid.item_id not in item_data:
@@ -42,6 +46,28 @@ def staff_notification():
                 statistics = api.get_status_counts(cid.id, iid,
                                                    ["staff", "peer", "done",
                                                     "waiting"])
+                modified_statistics = dict()
+                for stat in statistics:
+                    modified_statistics[stat.get('status')] = stat.get('count')
+
+                statistics = modified_statistics
+
+                if (( statistics['staff'] == 0 ) and ( statistics['peer'] == 0 ) and ( statistics['waiting'] == 0 )):
+                    return
+                    
+                
+                course_struct = None
+                chapter_name = None
+ 
+                try:
+                    course_struct = CourseStructure.objects.get(course_id=cid.id)
+                except Exception as e:
+                    print "Unexpected error {0}".format(e)
+
+                if course_struct:
+                    block = json.loads(course_struct.structure_json)['blocks'][iid]
+                    chapter_name = block['display_name']
+
                 staff_users = CourseAccessRole.objects.filter(course_id=cid.id,
                                                               role='staff')
                 try:
@@ -53,19 +79,24 @@ def staff_notification():
                     courseware_url = current_site_domain+"/courses/"+str(cid.id)+"/courseware/"+chapter+"/"+section
                     for u in staff_users:
                         html_message = render_to_string('peer_grading/ora_report.html',
-                                                        {'status_counts': statistics,
+                                                        {'status_counts': modified_statistics,
                                                          'course': cid.display_name,
+                                                         'chapter_name' : chapter_name,
                                                          'user': u.user,
                                                          'courseware_url':courseware_url
                                                          })
                         email = EmailMessage(
-                            "LYNX Online-Training: New Submissions for Staff Review", html_message,
+                            "LYNX Online-Training: Neue Aufgaben zur Bewertung", html_message,
                             to=[u.user.email])
                         email.send()
                         TASK_LOG.info("----------Email message sent to course admins----------")
                 except Exception as e:
                     TASK_LOG.info("----------Inner Exception while sending staff notification----------")
+                    import traceback
+                    print traceback.format_exc()
                     print e,"Inner Exception<-------"
                     pass
     except Exception as e:
+        import traceback
+        print traceback.format_exc() 
         print e,"<--- Error"
